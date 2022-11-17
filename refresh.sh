427b53aaf3e7c2034f346b2d0d160ed69b516f15
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2022-11-17 23:37:01 +0100

    refresh

--- refresh.sh
+++ refresh.sh
@@ -6,12 +6,13 @@ t=$W/Dockerfile.registryproxy
 git submodule init
 git submodule update
 cd docker-registry-proxy
+git remote add corpusops https://github.com/corpusops/docker-registry-proxy.git || true
 git fetch --all
 git pull origin master
-git reset --hard
+git reset --hard corpusops/ownauth
 egrep "^ARG BASE"  Dockerfile > "$t"
 cat ../Dockerfile.registryproxy.pre >> "$t"
 egrep -v "^(ARG BASE|VOLUME )"  Dockerfile |sed -re "s/ADD /ADD docker-registry-proxy\//g">> "$t"
-sed -i -re "/mkdir.*certs/d" create*sh
+sed -i -re "/^mkdir.*certs/d" create*sh
 cat ../Dockerfile.registryproxy.post >> "$t"
-# vim:set et sts=4 ts=4 tw=80:
+# vim:set et sts=4 ts=4 tw=0:
